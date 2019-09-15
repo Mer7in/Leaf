8541b1df77d870a5b5f30b129e6a3abe252705db
Mer7in <39715126+Mer7in@users.noreply.github.com>
2019-09-15 01:32:50 -0400

    Screen creation

---
+++ app/src/main/java/com/example/leaf/splashscreen.java
@@ -0,0 +1,68 @@
+package com.example.leaf;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Vector;
+
+public class splashscreen extends AppCompatActivity {
+
+    Vector
+    private Handler mHandler;
+    private Runnable mRunnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        mRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), splashscreen.class));
+            }
+        };
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable,2000);
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if(mHandler!=null && mRunnable !=null)
+            mHandler.removeCallbacks(mRunnable);
+    }
+}
