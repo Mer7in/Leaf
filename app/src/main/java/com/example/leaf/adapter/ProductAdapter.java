3a73896bc4835f378b2cbb045d0f21efb05d31e2
Mer7in <39715126+Mer7in@users.noreply.github.com>
2019-09-16 17:50:27 -0400

    Adpater - Model

---
+++ app/src/main/java/com/example/leaf/adapter/ProductAdapter.java
@@ -0,0 +1,10 @@
+package com.example.leaf.adapter;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+    Context context;
+    List<Product> product;
+}
