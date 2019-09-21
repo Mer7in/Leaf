942fde3661014cd23afae9474609c1d84c5adc8c
Mer7in <39715126+Mer7in@users.noreply.github.com>
2019-09-21 11:31:48 -0400

    RecyclerView setup - Fruit Fragment

--- app/src/main/java/com/example/leaf/fragment/fruit_fragment.java
+++ app/src/main/java/com/example/leaf/fragment/fruit_fragment.java
@@ -6,13 +6,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.leaf.DetailActivity;
+import com.example.leaf.Listener.ItemClickSupport;
 import com.example.leaf.Model.Product;
 import com.example.leaf.R;
 import com.example.leaf.adapter.ProductAdapter;
@@ -36,6 +36,35 @@ public class fruit_fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        queryFruit();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_fruit, container, false);
+        recyclerView= view.findViewById(R.id.fruitRecycleView);
+
+        this.configureOnClickRecyclerView();
+
+        return view;
+    }
+
+    private void configureOnClickRecyclerView(){
+        ItemClickSupport.addTo(recyclerView, R.layout.fragment_fruit)
+                .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        Intent i=new Intent(getContext(), DetailActivity.class);
+                        startActivity(i);
+                    }
+                });
+    }
+
+    public void queryFruit()
+    {
         ParseQuery<Product> query = new ParseQuery<Product>(Product.class);
         query.whereEqualTo("type","Fruit");
         query.orderByAscending("name");
@@ -45,8 +74,12 @@ public class fruit_fragment extends Fragment {
             @Override
             public void done(List<Product> product, ParseException e) {
                 if (e == null) {
+                    productAdapter=new ProductAdapter(getContext(),products);
                     products.clear();
                     products.addAll(product);
+
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    recyclerView.setAdapter(productAdapter);
                     Log.d("message",product.toString());
                     productAdapter.notifyDataSetChanged(); // update adapter
 
@@ -58,20 +91,11 @@ public class fruit_fragment extends Fragment {
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view= inflater.inflate(R.layout.fragment_fruit, container, false);
-        recyclerView= view.findViewById(R.id.fruitRecycleView);
-        productAdapter=new ProductAdapter(getContext(),products);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(productAdapter);
-
-        return view;
+    public void SearchFruit()
+    {
+        ParseQuery<Product> query =new ParseQuery<Product>(Product.class);
+        query.
     }
-
-
 
 
 }
